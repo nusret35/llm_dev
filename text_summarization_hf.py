d73ee810eb4b01d7539de8baecde8d079fd41482
selinceydeli <120125253+selinceydeli@users.noreply.github.com>
2023-07-03 10:05:43 +0300

    second commit nlp data preprocessing

--- text_summarization_hf.py
+++ text_summarization_hf.py
@@ -7,6 +7,18 @@ import re
 from pathlib import Path
 import os
 import string
+from nltk.corpus import stopwords
+
+# Function to format text
+def format_text(text):
+    new_text = ''
+    for char in text:
+        if char != r'\n':
+            new_text += char
+    sentences = re.split('\.\s*', new_text)
+    sentences = [s.strip().capitalize() for s in sentences if s]
+    formatted_text = '. '.join(sentences) + '.'
+    return formatted_text
 
 # Function to read and parse HTML file
 def read_html_file(file):
@@ -19,6 +31,7 @@ def read_html_file(file):
     text = '\n'.join(chunk for chunk in chunks if chunk)
     return text
 
+# Functions to conduct data cleaning
 def remove_html(text):
     html = re.compile(r"<.*?>")
     return html.sub(r"",text)
@@ -27,30 +40,14 @@ def remove_URL(text):
     url = re.compile(r"https?://\S+|www\.\S+")
     return url.sub(r"",text)
 
-
 def remove_punct(text):
     table = str.maketrans("","",string.punctuation)
     return text.translate(table)
 
-def format_text(text):
-    new_text = ''
-    for char in text:
-        if char != r'\n':
-            new_text += char
-    sentences = re.split('\.\s*', new_text)
-    sentences = [s.strip().capitalize() for s in sentences if s]
-    formatted_text = '. '.join(sentences) + '.'
-    return formatted_text
-
-# Initialize an empty string to store the summaries
-summary = ""
-
+# Initialize an empty array to append the documents as texts
 texts = [] 
 
 # Read the zip file
-#with zipfile.ZipFile("/Users/selinceydeli/Desktop/pages.zip", "r") as z:
-#with zipfile.ZipFile("./pages.zip", "r") as z:
-
 z =  Path('pages/')
 
 for directory in z.iterdir():
@@ -63,14 +60,11 @@ for directory in z.iterdir():
                     text = read_html_file(data)
                     texts.append(text)
 
-from nltk.corpus import stopwords
-
+# Conduct data cleaning
 stop = set(stopwords.words("english"))
-
 
 def remove_stopwords(text):
     text = [word.lower() for word in text.split() if word.lower() not in stop]
-
     return " ".join(text) 
 
 clean_texts = []
@@ -83,5 +77,5 @@ for text in texts :
     clean_texts.append(cleaned_text)
 
 
-print(len(texts))
-print(texts[1])
+print(len(clean_texts))
+print(clean_texts[0])
