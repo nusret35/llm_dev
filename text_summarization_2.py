46c2178f5aee249fdf15a7b50351e05ca67f8020
Nusret Ali Kizilaslan <nusret35@users.noreply.github.com>
2023-07-05 10:28:46 +0300

    google drive api is set

--- text_summarization_2.py
+++ text_summarization_2.py
@@ -1,10 +1,14 @@
 from data_collection import output_text
 from pathlib import Path
+from dataset_api import get_dataset
+
+'''
+GOOGLE_DRIVE_DATASET_FOLDER_ID = '1EyD75cYyLqRe0YlP7r556GkxIrDmv6Wy'
+dataset = get_dataset(GOOGLE_DRIVE_DATASET_FOLDER_ID)
+'''
 
 z = Path('datasets/')
 
 texts, targets = output_text(z)
 print(texts)
 print(targets)
-
-
