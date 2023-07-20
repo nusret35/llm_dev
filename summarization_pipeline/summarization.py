dccfe968c0e73ac869b20d7c6dcce5fa2e9adc0f
selinceydeli <120125253+selinceydeli@users.noreply.github.com>
2023-07-20 15:53:44 +0300

    summarization pipeline partitioning the text by sections is complete

--- summarization_pipeline/summarization.py
+++ summarization_pipeline/summarization.py
@@ -1,27 +1,35 @@
 from transformers import pipeline
 import torch
+import re
 
 # Load the summarization pipeline
+"""
 summarizer = pipeline(
     "summarization",
     "pszemraj/long-t5-tglobal-base-16384-book-summary",
     device=0 if torch.cuda.is_available() else -1,
 )
+"""
 
-section_names = ['Climate Change', 'Author', 'Abstract', 'Introduction', 'Methodology', 'Trends in Climate Change', 'Consequences of Climate Change', 'Mitigation Strategies', 'Outcomes', 'Conclusion', 'Keywords', 'Acknowledgements', 'References']
+summarizer = pipeline("summarization")
+
+section_names = ['Climate Change:', 'Author:', 'Abstract:', 'Introduction:', 'Methodology:', 'Trends in Climate Change:', 'Consequences of Climate Change:', 'Mitigation Strategies:', 'Outcomes:', 'Conclusion:', 'Keywords:', 'Acknowledgements:', 'References:']
 
 with open('summarization_pipeline/data.txt', 'r') as file:
-    lines = file.readlines()
+    text = file.read()
 
-summary = ""
+sections = re.split('\d+\..+\n', text)  # Split on lines that start with "number.title"
+sections = [section.strip() for section in sections if section.strip()]
 
-for line in lines:
-    line = line.strip()  # remove any leading/trailing whitespace
-    if line not in section_names and line != "":
-        summary += summarizer(line, max_length=50, min_length=25, do_sample=False)[0]['summary_text']
+print(len(sections))
 
-# Note that summarizer returns a list of dictionaries. So, to get the actual summary text, we need to access the 'summary_text' key of the first element in the list.
+summaries = []
 
+for section in sections:
+    section = section.strip()  # remove any leading/trailing whitespace
+    if section not in section_names and section != "":
+        # Deciding on the max_length and min_length parameters based on input length
+        summaries.append(summarizer(section, max_length=50, min_length=25, do_sample=False)[0]['summary_text'])
 
+print(len(summaries))
 
-#summarizer(chunk, max_length=max_length, min_length=0, do_sample=False)
