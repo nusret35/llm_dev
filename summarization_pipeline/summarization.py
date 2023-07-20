46721f78ea77e680f7d0cf1e1823162d53ac19dd
selinceydeli <120125253+selinceydeli@users.noreply.github.com>
2023-07-20 22:01:19 +0300

    summarization pipeline with recursive grouping of previous summaries is complete

--- summarization_pipeline/summarization.py
+++ summarization_pipeline/summarization.py
@@ -2,7 +2,6 @@ from transformers import pipeline
 import torch
 import re
 
-# Load the summarization pipeline
 """
 summarizer = pipeline(
     "summarization",
@@ -11,25 +10,63 @@ summarizer = pipeline(
 )
 """
 
+# Load the summarization pipeline
 summarizer = pipeline("summarization")
 
-section_names = ['Climate Change:', 'Author:', 'Abstract:', 'Introduction:', 'Methodology:', 'Trends in Climate Change:', 'Consequences of Climate Change:', 'Mitigation Strategies:', 'Outcomes:', 'Conclusion:', 'Keywords:', 'Acknowledgements:', 'References:']
-
-with open('summarization_pipeline/data.txt', 'r') as file:
+with open('summarization_pipeline/data2.txt', 'r') as file:
     text = file.read()
 
 sections = re.split('\d+\..+\n', text)  # Split on lines that start with "number.title"
 sections = [section.strip() for section in sections if section.strip()]
 
-print(len(sections))
-
 summaries = []
 
 for section in sections:
+    section = section.strip()  # Remove any leading/trailing whitespace
+    summaries.append(summarizer(section, max_length=50, min_length=20, do_sample=False)[0]['summary_text'])
+
+# I realized that we do not need to consider the subsections seperately. 
+# The redex for the sections already handle the seperation of the subsections. 
+# So I commented out the below for-loop. 
+
+"""
+for section in sections:
     section = section.strip()  # remove any leading/trailing whitespace
-    if section not in section_names and section != "":
-        # Deciding on the max_length and min_length parameters based on input length
-        summaries.append(summarizer(section, max_length=50, min_length=25, do_sample=False)[0]['summary_text'])
 
-print(len(summaries))
+    # Split the sections further into subsections
+    subsections = re.split('\d+\.\d+\..+\n', section)  # Split on lines that start with "number.number.title"
+    subsections = [subsection.strip() for subsection in subsections if subsection.strip()]
 
+    for subsection in subsections:
+        subsection = subsection.strip() 
+
+        # Split the subsections further into sub-subsections
+        subsubsections = re.split('\d+\.\d+\.\d+\..+\n', subsection)  # Split on lines that start with "number.number.number.title"
+        subsubsections = [subsubsection.strip() for subsubsection in subsubsections if subsubsection.strip()]
+
+        for subsubsection in subsubsections:
+            subsubsection = subsubsection.strip() 
+            summaries.append(summarizer(subsubsection, max_length=50, min_length=25, do_sample=False)[0]['summary_text'])
+"""
+
+# Define a function to handle the iterative grouping of summaries
+
+def recursive_grouping(summaries, max_length=70):
+    # Base case: if there's only one summary, return it
+    if len(summaries) == 1:
+        return summaries[0]
+
+    # If more than one summary, group them by two and generate new summaries
+    new_summaries = []
+    for i in range(0, len(summaries), 2):
+        text = summaries[i]
+        if i + 1 < len(summaries):  # Check to ensure we don't exceed the list index
+            text += " " + summaries[i + 1]
+        new_summaries.append(summarizer(text, max_length=max_length, min_length=20, do_sample=False)[0]['summary_text'])
+
+    return recursive_grouping(new_summaries, max_length + 10)  # Increase max_length for each new round
+
+final_summary = recursive_grouping(summaries)
+
+print()
+print(final_summary)
