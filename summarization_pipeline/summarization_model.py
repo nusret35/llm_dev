fd8f1e18ac528fa7a7c27720a4cc45c0c2aaf96a
Nusret Ali Kizilaslan <nusret35@users.noreply.github.com>
2023-07-29 18:19:06 +0300

    added summarize function to Summarizer class and organized the code

--- summarization_pipeline/summarization_model.py
+++ summarization_pipeline/summarization_model.py
@@ -1,4 +1,5 @@
 from transformers import pipeline
+from summarizer import Summarizer
 import torch
 import re
 import subprocess
@@ -11,32 +12,8 @@ summarizer = pipeline(
 )
 """
 
-def alpaca_model_summarization(input): 
-    prompt = 'Summarize this text: ' + input
-    # Path to the compiled C++ executable 'chat_app'
-    #cpp_executable = './alpaca-exec-s'
-    cpp_executable = './alpaca-exec-n'
-
-    # Prepare the command-line arguments for the C++ executable
-    # Replace any spaces or special characters in 'prompt' with underscores (_) 
-    prompt = prompt.replace(' ', '_').replace('-', '_').replace('@', '_')
-    args = [cpp_executable, '--prompt', prompt]
-
-    try:
-        # Run the C++ executable and capture the output
-        result = subprocess.run(args, capture_output=True, text=True, check=True)
-        # Extract the output from the subprocess
-        output = result.stdout.strip()
-        return output    
-    except subprocess.CalledProcessError as e:
-        print(f"Error executing C++ code: {e}")
-        print(f"Error message: {e.stderr}")
-        return None
-
-
 # Define a function to handle the iterative grouping of summaries
-
-def recursive_grouping(summaries, max_length=70):
+def recursive_grouping(summaries, summarizer, max_length=70):
     # Base case: if there's only one summary, return it
     if len(summaries) == 1:
         return summaries[0]
@@ -51,10 +28,10 @@ def recursive_grouping(summaries, max_length=70):
         # new_summaries.append(summarizer(text, max_length=max_length, min_length=20, do_sample=False)[0]['summary_text'])
 
         # Alpaca model summarization
-        summary = alpaca_model_summarization(text)
+        summary = summarizer.summarize(text)
         new_summaries.append(summary)
 
-    return recursive_grouping(new_summaries, max_length + 10)  # Increase max_length for each new round
+    return recursive_grouping(new_summaries, summarizer, max_length + 10)  # Increase max_length for each new round
 
 def divide_article_into_sections(article):
     sections = {}
@@ -77,14 +54,19 @@ def divide_article_into_sections(article):
 
 
 if __name__ == "__main__":
-    
-    # Load the summarization pipeline
-    # summarizer = pipeline("summarization")
+
+    #exec_path = './alpaca-exec-s'
+    exec_path = './alpaca-exec-n'
+
+
+    # Summarizer model
+    summarizer = Summarizer(model_path=exec_path)
 
     # Read txt document
     with open('summarization_pipeline/data2.txt', 'r') as file:
         text = file.read()
 
+    # Divide article into sections and return a dictionary (section names as keys and texts as values)
     sections = divide_article_into_sections(text)
 
     print(sections)
@@ -93,39 +75,17 @@ if __name__ == "__main__":
 
     for section in sections.values():
         section = section.strip()  # Remove any leading/trailing whitespace
+
         # Hugging Face pipeline summarization
         #summaries.append(summarizer(section, max_length=50, min_length=20, do_sample=False)[0]['summary_text'])
 
         # Alpaca model summarization
-        summary = alpaca_model_summarization(section)
+        summary = summarizer.summarize(section)
         print(summary)
         summaries.append(summary)
 
-    # I realized that we do not need to consider the subsections seperately. 
-    # The redex for the sections already handle the seperation of the subsections. 
-    # So I commented out the below for-loop. 
 
-    """
-    for section in sections:
-        section = section.strip()  # remove any leading/trailing whitespace
-
-        # Split the sections further into subsections
-        subsections = re.split('\d+\.\d+\..+\n', section)  # Split on lines that start with "number.number.title"
-        subsections = [subsection.strip() for subsection in subsections if subsection.strip()]
-
-        for subsection in subsections:
-            subsection = subsection.strip() 
-
-            # Split the subsections further into sub-subsections
-            subsubsections = re.split('\d+\.\d+\.\d+\..+\n', subsection)  # Split on lines that start with "number.number.number.title"
-            subsubsections = [subsubsection.strip() for subsubsection in subsubsections if subsubsection.strip()]
-
-            for subsubsection in subsubsections:
-                subsubsection = subsubsection.strip() 
-                summaries.append(summarizer(subsubsection, max_length=50, min_length=25, do_sample=False)[0]['summary_text'])
-    """
-
-    final_summary = recursive_grouping(summaries)
+    final_summary = recursive_grouping(summaries,summarizer)
 
     print()
     print(final_summary)
