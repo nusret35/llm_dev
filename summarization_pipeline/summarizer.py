c5e14d780ad51abc78e0fc604456acaf4a71c79d
Nusret Ali Kizilaslan <nusret35@users.noreply.github.com>
2023-10-31 22:04:14 +0300

    again minor fix

--- summarization_pipeline/summarizer.py
+++ summarization_pipeline/summarizer.py
@@ -2,10 +2,8 @@ import subprocess
 from transformers import pipeline
 import torch
 
-
 class Summarizer : 
 
-<<<<<<< HEAD
     def __init__(self,exec_path=None):
         self.exec_path = exec_path
 
@@ -14,24 +12,11 @@ class Summarizer :
         output = pipe(prompt)
         print(output)
 
-    def _make_prompt(self,instruction):
-        prompt = "Below is an instruction that describes a task. Write a response that appropriately completes the request\n### Instruction:" + instruction + "\n### Response:"
-        return prompt
-    
-    def _make_prompt_2(self,instruction, input):
-        prompt = "### Instruction:" + instruction  + "\n### Input: "+ input + "\n### Response:"
-        return prompt
-    
-=======
->>>>>>> 63073e24071f726972a426f545456345c06e3786
     def _send_prompt(self,prompt):
         #./main -m ./models/7B/./ggml-model-q4_0.bin -n 1024 --repeat_penalty 1.0 --color -ins -f ./prompts/summarization2.txt
         #model_path = '/Users/nusretkizilaslan/Desktop/AIProject/llama2/llama.cpp/models/./7B/ggml-vocab-q4_0.bin'
-        #model_path = '/Users/selinceydeli/Desktop/llama/llama.cpp/models/7B/ggml-model-q4_0.bin'
-        #alpaca_path = "/Users/selinceydeli/Desktop/llama/llama.cpp/models/alpaca.13b.ggmlv3.q8_0.bin"
-        alpaca_path = '/Users/nusretkizilaslan/Desktop/AIProject/llama2/llama.cpp/models/alpaca.13b.ggmlv3.q8_0.bin'
-        args = [self.exec_path, '-m', alpaca_path, '--color','-p',prompt,'--ctx_size','4096','-n','-1', '-b','256','--top_k','10000','--temp', '0.2','--repeat_penalty','1.1','-t','7']
-        
+        model_path = '/Users/selinceydeli/Desktop/llama/llama.cpp/models/7B/ggml-model-q4_0.bin'
+        args = [self.exec_path, '-m', model_path, '-n', '1024', '--repeat_penalty','1.0', '--color', '-ins', '-f', prompt]
         try:
             # Run the C++ executable and capture the output
             result = subprocess.run(args, capture_output=True, text=True, check=True)
@@ -47,59 +32,19 @@ class Summarizer :
     def prompt_generator(self,instruction,input):
         prompt = 'Instruction: ' + instruction + ".  Input: " + input 
         return prompt
-    
-    def return_response(self, output):
-        position = output.rfind("### Response:")
-        if position != -1:  # Check whether "### Response:" is found
-            position += len("### Response:")  # Move position after "### Response:"
-            response = output[position:]  # Slice the string from the new position to the end
-            return response.strip()  # Strip is used to remove any leading/trailing whitespace or newlines
-        else:
-            return "### Response: not found in the text"
 
     def summarize(self,text):
-        instruction = "Summarize this text: " + text
-        #prompt = self._make_prompt(instruction)
-        output = self._send_prompt(instruction)
-        response = self.return_response(output)
-        return response
+        length = len(text.split())
+        summary_limit = max(30, int(length*0.5))
+        prompt = "Summarize this text with maximum " + str(summary_limit) + " words: " + text
+        output = self._send_prompt(prompt)
+        return output
 
     def find_thesis_statament(self,text):
-        instruction = 'What is the thesis statement of this text: ' + text
-        prompt = self._make_prompt(instruction)
+        prompt = 'What is the thesis statement of this text: ' + text
         output = self._send_prompt(prompt)
-        response = self.return_response(output)
-        return response
+        return output
     
-    def enrich_abstract_1(self,abstract,sections):
-        prompt = '### Instruction:Expand the abstract based on the important information from the given Introduction, Conclusion, Methodology, and Outcomes. Return the enlarged abstract.'
-        prompt = prompt + '\n' + '### Input:'
-        prompt = prompt + '\n' + '- Abstract: ' + abstract
-        for section, text in sections.items():
-            if text != None:
-                prompt = prompt + '\n- ' + section + ': ' + text
-                print('\n')
-                print('\n')
-        prompt += '### Response: '
-        print(prompt)
-        output = self._send_prompt(prompt)
-        response = self.return_response(output)
-        return response
-    
-    def enrich_abstract_2(self,abstract,sections):
-        base_instruction = "Expand the abstract based on the important information from the given section texts. Return the enlarged abstract."
-        #base_instruction = "Concatenate the absract with the important information from the given section texts. Return the concatenated abstract."
-        instruction = 'Abstract: '+ abstract
-        for section, text in sections.items():
-            if text != None:
-                instruction += '\\' + section + ':' + text 
-        instruction += base_instruction
-        prompt = self._make_prompt(instruction)
-        #prompt = instruction
-        output = self._send_prompt(prompt)
-        response = self.return_response(output)
-        return response
-
     # Returns the text of a section together with the text of its subsections
     def section_text(self, section_name, sections_dict):
         # Convert section_name to lowercase for case-insensitive comparison
@@ -126,7 +71,7 @@ class Summarizer :
         abstract = self.section_text('Abstract',sections_dict)
         introduction = self.section_text('Introduction',sections_dict)
         conclusion = self.section_text('Conclusion',sections_dict)
-        
+
         prompt = 'I have an article at hand, whose title is: ' + title + 'Whose abstract is: ' + abstract + 'Whose introduction is: ' + introduction + 'Whose conclusion is: ' + conclusion + '/n What did the author set out to do and what was the outcome?'
         output = self._send_prompt(prompt)
         return output
