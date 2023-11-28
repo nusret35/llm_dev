7607edf0cb1631b9806a9618e22ad6447383a93a
Nusret Ali Kizilaslan <nusret35@users.noreply.github.com>
2023-11-28 17:09:13 +0300

    extracted images are mapped to their corresponding titles

--- summarization_pipeline/image_processing.py
+++ summarization_pipeline/image_processing.py
@@ -1,15 +1,17 @@
 import fitz
-import io
 from PIL import Image
 import os
 
-def extract_image_from_page(page,page_index):
-    image_list = page.get_image_info()
 
-    if image_list:
-        print(f"[+] Found a total of {len(image_list)} images in page {page_index}")
-    else:
-        print("[!] No images found on page", page_index)
+def extract_image_from_page(page, page_index):
+    image_list = page.get_image_info()
+    text_blocks = page.get_text("blocks")
+
+    keywords = ['table', 'figure', 'fig.', 'chart']
+    proximity_threshold = 50  # Adjust this value based on your requirements
+
+    image_titles = []
+    image_paths = []
 
     img_index = 0
     for img in image_list:
@@ -22,14 +24,36 @@ def extract_image_from_page(page,page_index):
         # Convert the image data to a Pillow image
         pil_image = Image.frombytes("RGB", [image.width, image.height], image.samples)
 
-        directory_path = "./images/"+ 'page' +str(page_index) 
+        directory_path = "./images/" + 'page' + str(page_index)
         os.makedirs(directory_path, exist_ok=True)
 
-        img_name =  directory_path +'/output_image' + str(img_index) + '.png'
+        img_path = directory_path + '/output_image' + str(img_index) + '.png'
+        image_paths.append(img_path)
+
         # Save or process the image as needed
-        pil_image.save(img_name)
+        pil_image.save(img_path)
+
+        # Search for text near the image
+        for block in text_blocks:
+            block_bbox = block[0:4]
+            block_left, block_top, block_right, block_bottom = block_bbox
+
+            # Check if the text block is within proximity of the image
+            if (left - proximity_threshold <= block_right <= right + proximity_threshold) and \
+               (top - proximity_threshold <= block_bottom <= bottom + proximity_threshold):
+
+                # Check if the first word of the text block is in the list of keywords
+                block_text = block[4]
+                first_word = block_text.strip().split()[0].lower()
+                if first_word in keywords:
+                    image_titles.append(block_text.strip())
+    
+    image_data = dict(zip(image_titles, image_paths))
+    return image_data
+    
 
 # File path
+
 #file = "/Users/selinceydeli/Desktop/AIResearch/business-article-inputs/1-s2.0-S0148296323004216-main.pdf"
 file = '/Users/nusretkizilaslan/Downloads/selo-article.pdf'
 
@@ -39,15 +63,12 @@ output_folder = "/Users/selinceydeli/Desktop/AIResearch/llm_dev/summarization_pi
 # Open the file
 pdf_file = fitz.open(file)
 
-import fitz  # PyMuPDF
-from PIL import Image
-
-
-
 # Iterate over PDF pages
 for page_index in range(len(pdf_file)):
     page = pdf_file[page_index]
-    extract_image_from_page(page,page_index)
+    page_image_data = extract_image_from_page(page,page_index)
+    print(page_image_data)
+    
     
 
 pdf_file.close()
