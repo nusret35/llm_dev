4f79b194a7b8abde7dbcefaa54f96d39d12eb0e2
Nusret Ali Kizilaslan <nusret35@users.noreply.github.com>
2024-03-18 16:24:03 +0300

    latest

--- backend.py
+++ backend.py
@@ -34,7 +34,7 @@ app.add_middleware(
     allow_headers=["*"],  # You might want to restrict this to specific headers
 )
 
-DEBUG_MODE = True
+DEBUG_MODE = False
 
 @app.websocket("/")
 async def websocket_interaction(websocket: WebSocket):
